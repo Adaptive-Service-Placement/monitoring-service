2ff5633866a1713697b907fc6f55442ed41ffdc6
fsix <six@campus.tu-berlin.de>
2023-04-04 15:47:59 +0200

    test commit for debugging purpose in early stages

--- src/main/java/com/example/monitoringservice/ApiConsumerRequestService.java
+++ src/main/java/com/example/monitoringservice/ApiConsumerRequestService.java
@@ -8,7 +8,7 @@ import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.Configuration;
 import io.kubernetes.client.openapi.apis.CoreV1Api;
 import io.kubernetes.client.openapi.models.V1NodeList;
-import io.kubernetes.client.util.Config;
+import io.kubernetes.client.util.ClientBuilder;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -70,7 +70,7 @@ public class ApiConsumerRequestService {
     private int determineNumberOfAvailableKubernetesNodes() {
         ApiClient client = null;
         try {
-            client = Config.defaultClient();
+            client = ClientBuilder.cluster().build();
         } catch (IOException e) {
             e.printStackTrace();
         }
