50618cc21af4a2d946d99fd57c471dd8ca9a28e1
fsix <six@campus.tu-berlin.de>
2023-04-08 22:20:56 +0200

    test commit for debugging purpose only (4)

--- src/main/java/com/example/monitoringservice/PodNodeAffinityHandler.java
+++ src/main/java/com/example/monitoringservice/PodNodeAffinityHandler.java
@@ -1,12 +1,15 @@
 package com.example.monitoringservice;
 
+import com.google.gson.reflect.TypeToken;
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.apis.CoreV1Api;
 import io.kubernetes.client.openapi.models.*;
+import io.kubernetes.client.util.Watch;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
 
 public class PodNodeAffinityHandler {
 
@@ -46,14 +49,32 @@ public class PodNodeAffinityHandler {
                     pod.setSpec(pod.getSpec().affinity(affinity));
                 }
 
+                replacePodOnceTerminated(pod);
 //                api.replaceNamespacedPod(pod.getMetadata().getName(), "default", pod, null, null, null, null);
-                api.deleteNamespacedPod(pod.getMetadata().getName(), "default", null, null, null, null, null, null);
-
-                api.createNamespacedPod("default", pod, null, null, null, null);
             }
             index++;
         }
     }
+
+    private void replacePodOnceTerminated(V1Pod pod) throws ApiException {
+        api.deleteNamespacedPod(Objects.requireNonNull(pod.getMetadata()).getName(), "default", null, null, null, null, null, null);
+
+        Watch<V1Pod> watch = Watch.createWatch(api.getApiClient(),
+                api.listNamespacedPodCall("default", null, null, null, null, null, null, null, null, 10, true, null),
+                new TypeToken<Watch.Response<V1Pod>>() {
+                }.getType());
+
+        for (Watch.Response<V1Pod> event : watch) {
+            V1Pod updatedPod = event.object;
+            V1ObjectMeta meta = updatedPod.getMetadata();
+            if (Objects.equals(Objects.requireNonNull(meta).getName(), Objects.requireNonNull(pod.getMetadata()).getName())) {
+                if ("Succeeded".equals(Objects.requireNonNull(updatedPod.getStatus()).getPhase())) {
+                    api.createNamespacedPod("default", pod, null, null, null, null);
+                }
+            }
+        }
+    }
+
 
     private void setLabel(V1Pod pod, String key, String value) {
         if (pod.getMetadata() != null && pod.getMetadata().getLabels() != null) {
