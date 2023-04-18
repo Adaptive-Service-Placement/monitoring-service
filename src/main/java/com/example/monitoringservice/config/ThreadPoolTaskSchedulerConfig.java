e0e73054c95bf2e48c121cb68dd00ac63b8c34e8
fsix <six@campus.tu-berlin.de>
2023-04-18 18:02:36 +0200

    adjusted scheduling

--- src/main/java/com/example/monitoringservice/config/ThreadPoolTaskSchedulerConfig.java
+++ src/main/java/com/example/monitoringservice/config/ThreadPoolTaskSchedulerConfig.java
@@ -1,11 +1,21 @@
 package com.example.monitoringservice.config;
 
+import com.example.monitoringservice.ExecuteMigrationTask;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+import java.time.Duration;
+
+@EnableScheduling
 @Configuration
-public class ThreadPoolTaskSchedulerConfig {
+public class ThreadPoolTaskSchedulerConfig implements SchedulingConfigurer {
+
+    private final String fixedRate = System.getenv("MIGRATION_INTERVAL") != null ? System.getenv("MIGRATION_INTERVAL") : "24";
+
     @Bean
     public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
         ThreadPoolTaskScheduler threadPoolTaskScheduler
@@ -13,5 +23,10 @@ public class ThreadPoolTaskSchedulerConfig {
         threadPoolTaskScheduler.setThreadNamePrefix(
                 "ThreadPoolTaskScheduler");
         return threadPoolTaskScheduler;
+    }
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.addFixedRateTask(new ExecuteMigrationTask(), Duration.ofHours(Long.parseLong(fixedRate)));
     }
 }
